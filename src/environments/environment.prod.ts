43e992eb741494a04efef6030f3e91894cb49163
Bien Tu Anh <bientuanh46@gmail.com>
2020-10-15 21:59:54 +0700

    Update files.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: 'https://bientuanh.github.io/sale-cources-app/',
-  baseApi: 'https://fake-rest-api-anhbt.herokuapp.com/',
+  baseApi: 'https://fake-rest-api-anhbt.herokuapp.com',
 };
